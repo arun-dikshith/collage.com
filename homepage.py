981d06c75b5aecb4aa8a2150b2234ecb157ccc03
arun-git363 <arun.balusani@gmail.com>
2025-08-21 17:45:18 +0000

    first commit in trr branch

--- homepage.py
+++ homepage.py
@@ -2,4 +2,8 @@
 <html>
   <head><title>My First Page</title></head>
   <body><h1>Hello, World!</h1></body>
+<head><title>Top collage in Hyderabad</title></head>
+  <body><h1>WELLCOME TO TRR ENGINEERING COLLAGE!</h1></body>
+    
+
 </html>
